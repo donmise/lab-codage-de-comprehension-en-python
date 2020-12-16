e5322f1feccbbb9bdcb95fe146ac4764c02be99c
donmise <ddmtchoupou@gmail.com>
2020-12-16 13:18:50 -0500

    Ajout de la fonction exo_04() conversion en ensemble

--- 04/programme.py
+++ 04/programme.py
@@ -8,7 +8,7 @@ def exo_04(liste):
   # Ajouter le code permettant de convertir
   # la liste en ensemble (set)
   # pensez au (set comprehension)
-  ensemble = ??? 
+  ensemble ={x for x in liste} 
   return sorted(ensemble)
 
 def main():
